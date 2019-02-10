0fe352180fc655743565fc0827e77428f4a7a8cc
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2019-02-10 11:02:02 +0900

    [fix] use 2 spaces as indent.

--- public/main.js
+++ public/main.js
@@ -4,13 +4,11 @@ import init from '/src/init.js';
 import swit from './src/swit.js';
 import cloudStorage from './src/cloud_storage.js';
 
-window.onload = () => {
-  // loading initialize once
-  init().then(me => {
-    cloudStorage.gameDataOnUpdate(data => {
-      if (data) {
-        swit(data, me);
-      }
-    });
-  })
-};
+// loading initialize once
+init().then(me => {
+  cloudStorage.gameDataOnUpdate(data => {
+    if (data) {
+      swit(data, me);
+    }
+  });
+});
